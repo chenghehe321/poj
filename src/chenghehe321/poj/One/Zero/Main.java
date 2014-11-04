13f47bf3e8c606656e029f2dc22d84bcf2a0e764
chenghehe321 <chengy1013@gmail.com>
2014-11-04 14:59:53 +0800

    1001

---
+++ src/chenghehe321/poj/One/Zero/Main.java
@@ -0,0 +1,18 @@
+package chenghehe321.poj.One.Zero;
+
+import java.util.Scanner;
+
+/**
+ * @author Carl
+ * 
+ *         2014-11-4
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		int a = cin.nextInt(), b = cin.nextInt();
+		System.out.println(a + b);
+	}
+
+}
